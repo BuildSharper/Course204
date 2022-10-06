abc55c0032ae3fae06fe09071622665e4b2b27ca
BuildSharper <buildsharper@gmail.com>
2022-10-05 23:44:32 -0500

    Added more error handling

--- BuildSharper.Course204.App/Program.cs
+++ BuildSharper.Course204.App/Program.cs
@@ -4,21 +4,38 @@ using Flurl.Http;
 
 var baseUrl = ConfigurationManager.AppSettings["WeatherStackBaseUrl"];
 var apiKey = ConfigurationManager.AppSettings["WeatherStackApiKey"];
+var done = false;
 
-Console.WriteLine("********************************************");
-Console.WriteLine("BuildSharper.com - Weather");
-Console.WriteLine("********************************************");
-Console.WriteLine("");
-Console.Write("Enter your city: ");
-var query = Console.ReadLine();
+while (!done)
+{
+    Console.WriteLine("********************************************");
+    Console.WriteLine("BuildSharper.com - Weather");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("");
+    Console.Write("Enter your city: ");
+    var query = Console.ReadLine();
 
-var apiUrl = $"{baseUrl}/current?access_key={apiKey}&units=f&query={query}";
+    var apiUrl = $"{baseUrl}/current?access_key={apiKey}&units=f&query={query}";
 
-Console.WriteLine("Fetching weather...");
+    Console.WriteLine("Fetching weather...");
 
-var response = await apiUrl.GetJsonAsync<WeatherStackResponse>();
+    var response = await apiUrl.GetJsonAsync<WeatherStackResponse>();
 
-Console.WriteLine($"{response.Location.Name}, {response.Location.Region} Local Weather: {response.Current.Weather_descriptions.FirstOrDefault()} {response.Current.Temperature} degrees");
+    if (response != null
+        && response.Current != null
+        && response.Location != null)
+    {   
+        done = true;
+        Console.WriteLine($"{response.Location.Name}, {response.Location.Region} Local Weather: {response.Current.Weather_descriptions.FirstOrDefault()} {response.Current.Temperature} degrees");
+    }
+    else
+    {
+        Console.WriteLine("Invalid city name, please try again.");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
+
 Console.WriteLine("");
 Console.WriteLine("Press any key to continue.");
 Console.ReadKey();
